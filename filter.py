19836a1287f72f99d2a8f6e14646a6e1ab153c09
gfixler <gary@garyfixler.com>
2020-02-21 02:54:28 -0800

    Rename Filter to Pred in filter.py
    I realized it's not a Filter; it's just a Predicate. It only operates
    [or should only] on a single argument, returning a boolean.

--- filter.py
+++ filter.py
@@ -9,22 +9,22 @@ eq  = lambda n: lambda x: x == n
 gte = lambda n: lambda x: x >= n
 gt  = lambda n: lambda x: x >  n
 
-isLT = lambda n: Filter(lt(n))
-isEq = lambda n: Filter(eq(n))
-isGT = lambda n: Filter(gt(n))
+isLT = lambda n: Predicate(lt(n))
+isEq = lambda n: Predicate(eq(n))
+isGT = lambda n: Predicate(gt(n))
 
 
-class Filter (object):
+class Predicate (object):
 
     def __init__ (self, pred):
         self.pred = pred
-        self.run = pred # a comfy alias
+        self.run  = pred # a comfy alias
 
     def __and__ (self, other):
-        return Filter(lambda x: self.run(x) and other.run(x))
+        return Predicate(lambda x: self.run(x) and other.run(x))
 
     def __or__ (self, other):
-        return Filter(lambda x: self.run(x) or other.run(x))
+        return Predicate(lambda x: self.run(x) or other.run(x))
 
 
 """
