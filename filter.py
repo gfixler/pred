28c00583682cfcc01ccfbf9ae1d8da31a1fdbce4
gfixler <gary@garyfixler.com>
2020-02-21 02:40:48 -0800

    Add filter.py with composable filters concept
    It occurred to me today that filtering would be really nice if I could
    simply compose filters together, and then run them, especially for that
    fusion (i.e. do all the work on one thing before moving on, no
    duplicating of lists for every filtration). This would be cake in
    Haskell, but it took a little bit of head-scratching in Python, and a
    mix of OO and FP ideas, because types in Python are basically classes.

---
+++ filter.py
@@ -0,0 +1,44 @@
+"""
+This script is a test bed for my composable filters concept.
+"""
+
+# example
+lt  = lambda n: lambda x: x <  n
+lte = lambda n: lambda x: x <= n
+eq  = lambda n: lambda x: x == n
+gte = lambda n: lambda x: x >= n
+gt  = lambda n: lambda x: x >  n
+
+isLT = lambda n: Filter(lt(n))
+isEq = lambda n: Filter(eq(n))
+isGT = lambda n: Filter(gt(n))
+
+
+class Filter (object):
+
+    def __init__ (self, pred):
+        self.pred = pred
+
+    def run (self, input):
+        return self.pred(input)
+
+    def __and__ (self, other):
+        return Filter(lambda x: self.run(x) and other.run(x))
+
+    def __or__ (self, other):
+        return Filter(lambda x: self.run(x) or other.run(x))
+
+
+"""
+# examples of composition, usage
+
+# compose filters
+(isLT(2) | isGT(6) | isEq(4)).run(3)
+
+# map composed filters
+map((isLT(2) | isGT(6) | isEq(4)).run, xrange(10))
+
+# filter with composed filters
+filter((isLT(2) | isGT(6) | isEq(4)).run, xrange(10))
+"""
+
