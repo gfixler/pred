3e88261be6c5a7d4fb98834e0a54942855da2f6b
gfixler <gary@garyfixler.com>
2020-02-27 02:32:00 -0800

    Make Pred callable
    Now Pred can be used directly as the original function, instead of
    having to access a property to retrieve it first. Much simpler.

--- filter.py
+++ filter.py
@@ -2,14 +2,17 @@ class Pred (object):
 
     def __init__ (self, pred):
         self.pred = pred
-        self.run  = pred # a comfy alias
 
     def __and__ (self, other):
-        return Pred(lambda x: self.run(x) and other.run(x))
+        return Pred(lambda x: self.pred(x) and other.pred(x))
 
     def __or__ (self, other):
-        return Pred(lambda x: self.run(x) or other.run(x))
+        return Pred(lambda x: self.pred(x) or other.pred(x))
 
     def __neg__ (self):
-        return Pred(lambda x: not self.run(x))
+        return Pred(lambda x: not self.pred(x))
+
+    def __call__ (self, *args):
+        return self.pred(*args)
+
 
