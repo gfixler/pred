da04ac31975723ab303d8155f4e8cb489cbeb515
gfixler <gary@garyfixler.com>
2020-05-11 05:05:39 -0700

    Add ast printer method to Pred
    I spiked this in a REPL.

--- filter.py
+++ filter.py
@@ -21,3 +21,12 @@ class Pred (object):
     def __neg__ (self):
         return Pred(op="NOT", right=self)
 
+    def ast (self, indent=0):
+        if self._op == "PRED":
+            return ("  " * indent) + str(self._pred) + "\n"
+        elif self._op == "AND":
+            return ("  " * indent) + "AND\n" + self._left.ast(indent=indent+2) + self._right.ast(indent=indent+2)
+        elif self._op == "OR":
+            return ("  " * indent) + "OR\n" + self._left.ast(indent=indent+2) + self._right.ast(indent=indent+2)
+        elif self._op == "NOT":
+            return ("  " * indent) + "NOT\N" + self._right.ast(indent=indent+2)
