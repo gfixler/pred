ad7302233d19857647ddfbcd3bd0d4745cea4b80
gfixler <gary@garyfixler.com>
2020-05-16 18:15:02 -0700

    Improve simplifiers

--- filter.py
+++ filter.py
@@ -14,6 +14,16 @@ class Pred (object):
         elif self._op == "OR":   return self._left(x) or self._right(x)
         elif self._op == "NOT":  return not self._pred(x)
 
+    def __eq__ (self, other):
+        if self._op != other._op:
+            return False
+        if self._op == "PRED":
+            return self._name == other._name
+        elif self._op == "NOT":
+            return self._pred == other._pred
+        elif self._op in ["AND", "OR"]:
+            return self._left == other._left and self._right == other._right
+
     def __and__ (self, other):
         p = Pred()
         p._op = "AND"
@@ -56,4 +66,48 @@ class Pred (object):
             return leftfn(self._left) + " & " + rightfn(self._right)
         elif self._op == "OR":
             return str(self._left) + " | " + str(self._right)
+
+
+true = Pred(lambda _: True, name="True")
+false = Pred(lambda _: False, name="False")
+
+def simplify_notNot (pred):
+    if pred._op == "NOT" and pred._pred._op == "NOT":
+        return (simplify(pred._pred._pred), True)
+    return (pred, False)
+
+def simplify_xAndNotX (pred):
+    if pred._op == "AND":
+        if pred._left._op == "NOT" and simplify(pred._right) == simplify(pred._left._pred):
+            return (false, True)
+        elif pred._right._op == "NOT" and simplify(pred._left) == simplify(pred._right._pred):
+            return (false, True)
+    return (pred, False)
+
+def simplify_xOrNotX (pred):
+    if pred._op == "OR":
+        if pred._left._op == "NOT" and simplify(pred._right) == simplify(pred._left._pred):
+            return (true, True)
+        elif pred._right._op == "NOT" and simplify(pred._left) == simplify(pred._right._pred):
+            return (true, True)
+    return (pred, False)
+
+def simplify_xAndOrX (pred):
+    if pred._op in ["AND", "OR"]:
+        simpleLeft = simplify(pred._left)
+        if simpleLeft == simplify(pred._right):
+            return (simpleLeft, True)
+    return (pred, False)
+
+def simplify (pred):
+    simplifiers = [ simplify_notNot
+                  , simplify_xAndNotX
+                  , simplify_xOrNotX
+                  , simplify_xAndOrX
+                  ]
+    for simplifier in simplifiers:
+        (result, status) = simplifier(pred)
+        if status:
+            return result
+    return pred
 
