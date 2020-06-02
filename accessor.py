d4ef853f1cf399c76f3c7fabcba62e9678c223b9
gfixler <gary@garyfixler.com>
2020-06-02 00:36:45 -0700

    Add accessor.StringAccessor, tests
    I'm pretending at types here. This lets you get a string, by calling a
    function, testing that you actually got a string, and imbuing the result
    with a bunch of string predicate creator methods.

--- accessor.py
+++ accessor.py
@@ -1,3 +1,9 @@
+import re
+
+
+from .filter import Pred
+
+
 class Accessor (object):
 
     def __init__ (self, accFunc):
@@ -5,4 +11,32 @@ class Accessor (object):
 
     def __call__ (self, *args):
         return self._accFunc(*args)
+
+
+class StringAccessor (Accessor):
+
+    def __call__ (self, *args):
+        result = self._accFunc(*args)
+        if type(result) != str:
+            try:
+                raise TypeError, "StringAccessor accessed non-string object: " + str(result)
+            except:
+                # supposedly str can fail (https://stackoverflow.com/a/4857604/955926)
+                raise TypeError, "StringAccessor accessed non-string object"
+        return result
+
+    def equals (self, string):
+        return Pred(lambda s: self(s) == string)
+
+    def contains (self, string):
+        return Pred(lambda s: string in self(s))
+
+    def startswith (self, string):
+        return Pred(lambda s: self(s).startswith(string))
+
+    def endswith (self, string):
+        return Pred(lambda s: self(s).endswith(string))
+
+    def matches (self, pattern):
+        return Pred(lambda s: re.match(pattern, s))
 
--- test/test_accessor.py
+++ test/test_accessor.py
@@ -16,3 +16,36 @@ class Test_Accessor (unittest.TestCase):
         data = {"a": 3, "b": 42, "c": 7}
         self.assertEquals(Accessor(lambda d: d["b"])(data), 42)
 
+
+class Test_StringAccessor (unittest.TestCase):
+
+    def setUp (self):
+        self.data = {"name": "Bob"}
+        self.name = StringAccessor(lambda d: d["name"])
+
+    def test_identityOnStringWorks (self):
+        self.assertEquals(StringAccessor(ident)("bar"), "bar")
+
+    def test_identityOnNonStringRaises (self):
+        self.assertRaises(TypeError, lambda: StringAccessor(ident)(42))
+
+    def test_canCreateIsPred (self):
+        self.assertTrue(self.name.equals("Bob")(self.data))
+        self.assertFalse(self.name.equals("Alice")(self.data))
+
+    def test_canCreateContainsPred (self):
+        self.assertTrue(self.name.contains("o")(self.data))
+        self.assertFalse(self.name.contains("e")(self.data))
+
+    def test_canCreateStartswithPred (self):
+        self.assertTrue(self.name.startswith("B")(self.data))
+        self.assertFalse(self.name.startswith("A")(self.data))
+
+    def test_canCreateEndswithPred (self):
+        self.assertTrue(self.name.endswith("b")(self.data))
+        self.assertFalse(self.name.endswith("e")(self.data))
+
+    def test_canCreateMatchesPred (self):
+        self.assertTrue(self.name.matches(".*\d\d-\d\d.*")("blue_23-42_hike"))
+        self.assertFalse(self.name.matches(".*\d\d-\d\d.*")("ab-cd"))
+
