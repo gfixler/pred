fd718acdb02585698fb9ecff02c89c152b7608e8
gfixler <gary@garyfixler.com>
2020-06-09 13:48:47 -0700

    Remove name, descrip, from Validator
    I may add these back in in some way later, but right now, they're
    interfering with what I'm trying to test about boolean combinators.

--- test/test_validate.py
+++ test/test_validate.py
@@ -11,28 +11,22 @@ const = lambda x: lambda _: x
 class Test_Validator (unittest.TestCase):
 
     def test_simplePredNoFix_passing (self):
-        name = "yay"
-        desc = "Any value would pass this."
         vdtr = const(True)
         fixr = None
-        v = Validator(name, desc, vdtr, fixr)
-        self.assertEquals(v(42), {"name": name, "descrip": desc, "status": "PASSED"})
+        v = Validator(vdtr, fixr)
+        self.assertEquals(v(42), {"status": "PASSED"})
 
     def test_simplePredBadFix_unfixed (self):
-        name = "uhoh"
-        desc = "No value would pass this."
         vdtr = const(False)
         fixr = ident
-        v = Validator(name, desc, vdtr, fixr)
-        self.assertEquals(v(42), {"name": name, "descrip": desc, "status": "UNFIXED"})
+        v = Validator(vdtr, fixr)
+        self.assertEquals(v(42), {"status": "UNFIXED"})
 
     def test_simplePredNoFix_unfixable (self):
-        name = "giveup"
-        desc = "Any value fails, and we have no fix."
         vdtr = const(False)
         fixr = None
-        v = Validator(name, desc, vdtr, fixr)
-        self.assertEquals(v(42), {"name": name, "descrip": desc, "status": "UNFIXABLE"})
+        v = Validator(vdtr, fixr)
+        self.assertEquals(v(42), {"status": "UNFIXABLE"})
 
     def test_simplePredNoFix_fixed (self):
         """
@@ -41,16 +35,14 @@ class Test_Validator (unittest.TestCase):
         """
         global globalvar
         globalvar = 3
-        name = "Actual fix to an incorrect, global var."
-        desc = "not so bad"
         def vdtr (_):
             global globalvar
             return globalvar == 42
         def fixr (_):
             global globalvar
             globalvar = 42
-        v = Validator(name, desc, vdtr, fixr)
-        self.assertEquals(v(3), {"name": name, "descrip": desc, "status": "FIXED"})
+        v = Validator(vdtr, fixr)
+        self.assertEquals(v(3), {"status": "FIXED"})
 
     def test_handlesFixThatRaises (self):
         """
@@ -60,13 +52,9 @@ class Test_Validator (unittest.TestCase):
         """
         def fixThatRaises (_):
             raise RuntimeError, "Fix blew up the world."
-        name = "exploder"
-        desc = "The fix raises."
         vdtr = const(False)
         fixr = fixThatRaises
-        v = Validator(name, desc, vdtr, fixr)
+        v = Validator(vdtr, fixr)
         result = v(42)
-        self.assertEquals(name, result["name"])
-        self.assertEquals(desc, result["descrip"])
         self.assertEquals("FIXRAISED", result["status"])
 
--- validate.py
+++ validate.py
@@ -1,31 +1,22 @@
 class Validator (object):
 
-    def __init__ (self, name, descrip, pred, fix):
-        self._name = name
-        self._descrip = descrip
+    def __init__ (self, pred, fix):
         self._pred = pred
         self._fix = fix
 
     def __call__ (self, x):
-        result = { "name": self._name
-                 , "descrip": self._descrip
-                 , "status": "NOTRUN"
-                 }
         if self._pred(x):
-            result["status"] = "PASSED"
+            return {"status": "PASSED"}
         else:
             if self._fix:
                 try:
                     self._fix(x)
                 except Exception as e:
-                    result["status"] = "FIXRAISED"
-                    result["error"] = e
-                    return result
+                    return {"status": "FIXRAISED", "error": e}
                 if self._pred(x):
-                    result["status"] = "FIXED"
+                    return {"status": "FIXED"}
                 else:
-                    result["status"] = "UNFIXED"
+                    return {"status": "UNFIXED"}
             else:
-                result["status"] = "UNFIXABLE"
-        return result
+                return {"status": "UNFIXABLE"}
 
